aab60b119953ea9307c132162c02bd1b5e5d2d51
Mavr1 <mavrodiy.m.e@gmail.com>
2020-06-20 15:42:38 +0300

    Updates

---
+++ precache-manifest.b5c27a47b52c4b92c4f3c82ccce352a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430c255de8e845e296dfef1aecb21c6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e98894b57ea1ff88998",
+    "url": "/static/css/main.cd71fc8e.chunk.css"
+  },
+  {
+    "revision": "b7d8dc47c840dd9ebb37",
+    "url": "/static/js/2.fd1b04d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd1b04d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e98894b57ea1ff88998",
+    "url": "/static/js/main.d81256e9.chunk.js"
+  },
+  {
+    "revision": "4ff1f0d5bce29dab4e96",
+    "url": "/static/js/runtime-main.3ce725fe.js"
+  }
+]);
